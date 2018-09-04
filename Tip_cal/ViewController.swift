bf04f6f7f3d66777ff058321de39c19eb3ffcb85
Claudia Martinez <claudiamartinez@claudias-macbook-pro.local>
2018-09-03 20:02:39 -0400

    Initial Commit

---
+++ Tip_cal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip_cal
+//
+//  Created by Claudia Martinez on 9/3/18.
+//  Copyright © 2018 Claudia Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
